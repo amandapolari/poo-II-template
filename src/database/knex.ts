662d1c0d47350a329dfd3690e02f286094a1f40c
Amanda <amandaoliveirapolari@gmail.com>
2023-10-25 19:35:53 -0300

    identação corrigida

--- src/database/knex.ts
+++ src/database/knex.ts
@@ -1,16 +1,16 @@
-import { knex } from "knex"
+import { knex } from 'knex';
 
 export const db = knex({
-    client: "sqlite3",
+    client: 'sqlite3',
     connection: {
-        filename: "./src/database/poo.db",
+        filename: './src/database/poo.db',
     },
     useNullAsDefault: true,
-    pool: { 
+    pool: {
         min: 0,
         max: 1,
         afterCreate: (conn: any, cb: any) => {
-            conn.run("PRAGMA foreign_keys = ON", cb)
-        }
-    }
-})
+            conn.run('PRAGMA foreign_keys = ON', cb);
+        },
+    },
+});
